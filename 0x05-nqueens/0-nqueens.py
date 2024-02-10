6386339941f0d349eec9c613684a8761179c7c4a
SarahEmmy <bawahsarah@gmail.com>
2024-02-09 18:06:08 -0800

    updates

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -2,18 +2,17 @@
 """ N queens """
 import sys
 
-
-if len(sys.argv) > 2 or len(sys.argv) < 2:
+if len(sys.argv) != 2:
     print("Usage: nqueens N")
-    exit(1)
+    sys.exit(1)
 
 if not sys.argv[1].isdigit():
     print("N must be a number")
-    exit(1)
+    sys.exit(1)
 
 if int(sys.argv[1]) < 4:
     print("N must be at least 4")
-    exit(1)
+    sys.exit(1)
 
 n = int(sys.argv[1])
 
